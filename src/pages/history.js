262bdff430453626ff473845de8c2d8da0d0d89e
admin <admin@MacBook-Pro-admin.local>
2019-06-15 17:08:16 +0300

    Настроили импорт стилей antd

--- src/pages/history.js
+++ src/pages/history.js
@@ -1,29 +1,45 @@
-import React, { Fragment } from 'react';
-import { Icon, Empty } from 'antd';
+import React, { Fragment, useState } from 'react';
+import { Icon, Empty, Pagination } from 'antd';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { REMOVE_PICTURE_FROM_HISTORY } from 'store/actionTypes';
-
 import styles from './History.less';
 
 const History = ({ removePicture, historyPictures }) => {
+  const pageSize = 5;
+  const [currentPage, setNewCurrentPage] = useState(1);
+  const from = pageSize * (currentPage - 1);
+  const to = pageSize * currentPage;
+  const historyPicturesOnCurrentPage = historyPictures.slice(from, to);
+  if (historyPicturesOnCurrentPage.length === 0 && currentPage > 1) {
+    setNewCurrentPage(currentPage - 1);
+  }
+
   const renderHistoryPic = () => (
     <Fragment>
       <div className={styles.historyContainer}>
         {
-          historyPictures.map(picture => (
-            <div className={styles.picContainer} key={picture.id}>
-              <div className={styles.imgContainer}>
-                <img align="left" src={picture.image_url} width="170px"/>
-                <Icon type="delete" onClick = {() => removePicture(picture.id)}/>
+          historyPicturesOnCurrentPage
+            .map(picture => (
+              <div className={styles.picContainer} key={picture.id}>
+                <div className={styles.imgContainer}>
+                  <img align="left" src={picture.image_url} width="170px"/>
+                  <Icon type="delete" onClick = {() => removePicture(picture.id)}/>
+                </div>
+                <div className={styles.textContainer}>
+                  <h3>{picture.title}</h3>
+                  <p>{picture.import_datetime}</p>
+                </div>
               </div>
-              <div className={styles.textContainer}>
-                <h3>{picture.title}</h3>
-                <p>{picture.import_datetime}</p>
-              </div>
-            </div>
-          ))
+            ))
         }
+        <Pagination
+          current={currentPage}
+          pageSize={pageSize}
+          total={historyPictures.length}
+          hideOnSinglePage
+          onChange={(newCurrentPage) => setNewCurrentPage(newCurrentPage)}
+        />
       </div>
     </ Fragment>
   );
